ddc39e18695bcfbc9432ad0cbca5c22337b4f712
DanJaegerDev <59783252+DanJaeger@users.noreply.github.com>
2020-11-09 21:37:09 -0500

    Creating InputManager

---
+++ MaxiFly DEMO/Assets/Scripts/Inputs Scripts/InputManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public void PassInput(InputData data)
+    {
+
+    }
+}
+
+public struct InputData
+{
+    public bool[] buttons;
+
+    public InputData(int buttonCount)
+    {
+        buttons = new bool[buttonCount];
+    }
+
+    public void Reset()
+    {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i] = false;
+        }
+    }
+
+}
---
+++ MaxiFly DEMO/Assets/Scripts/Inputs Scripts/KeyboardTracker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardTracker : DeviceTracker
+{
+    
+    private void Update()
+    {
+        //Check for Inputs
+        if (newData)
+        {
+            im.PassInput(data);
+            newData = false;
+            data.Reset();
+        }
+    }
+
+
+}
